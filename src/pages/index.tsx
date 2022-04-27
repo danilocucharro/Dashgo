e9307a8fc90248d8438944f1f7fba9b4308bbbe5
danilo cucharro <danicucharro@outlook.com>
2022-04-27 11:33:20 -0300

    restruturação do componente input do formulario da pagina de SignIn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Flex, Input, Button, Stack } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/input'
 
-export default function Home() {
+export default function SingIn() {
   return (
     <Flex
       w="100vw"
@@ -17,39 +18,12 @@ export default function Home() {
         borderRadius={8}
         flexDir="column"
       > 
-        <Stack spacing={4}>
-          <Input
-            name="email"
-            type="email"
-            focusBorderColor="pink.500"
-            bgColor="gray.900"
-            variant="filled"
-            _hover={{
-              bgColor: "gray.900"
-            }}
-            size="lg"
-          />
-
-          <Input
-            name="password"
-            type="password"
-            focusBorderColor="pink.500"
-            bgColor="gray.900"
-            variant="filled"
-            _hover={{
-              bgColor: "gray.900"
-            }}
-            size="lg"
-          />
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />        
         </Stack>
 
-        <Button
-          type="submit"
-          mt="6"
-          colorScheme="pink"
-        >
-          Entrar
-        </Button>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
       </Flex>
     </Flex>
   )
