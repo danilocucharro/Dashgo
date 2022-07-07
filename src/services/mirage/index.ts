f28756fc6c82273f5a3f7e6e2279ee02976db54a
danilo cucharro <danicucharro@outlook.com>
2022-07-07 11:43:29 -0300

    mirage junto com lib faker agora criam usuarios fakes para testes da aplicação pela chama da rota /users

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from 'miragejs'
+import { createServer, Factory, Model } from 'miragejs'
+import faker from 'faker' //biblioteca de geração de dados falsos
 
 type User = {//tipando o formato do user
     name: string;
@@ -11,6 +12,24 @@ export function makeServer(){//simulando um servidor backend com o miragejs
         models: {//declarando os models necessarios para o dashgo
             user: Model.extend<Partial<User>>({})//Partial: faz com que todos os campos do user nao sejam obrigatorios
         },
+
+        factories: {//gerar varios dados de uma vez, ao inves de criar varios schemas dentro da seeds
+            user: Factory.extend({
+                name(i: number) {
+                    return `User ${i + 1}`
+                },
+                email() {
+                    return faker.internet.email().toLowerCase();
+                },
+                createdAt() {
+                    return faker.date.recent(10);
+                },
+            })
+        },
+
+        seeds(server){
+            server.createList('user', 200)//criando 200 usuarios
+        }, 
 
         routes(){//o mirage identifica automaticamente a chamada de get e de post para a rota /users
             this.namespace = 'api';// setando o caminho que a aplicação precisa acessar para chamar o mirage
