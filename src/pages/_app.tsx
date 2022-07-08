9c4076b52d76a335db7af82e84779b7a31419f6f
danilo cucharro <danicucharro@outlook.com>
2022-07-08 10:22:11 -0300

    agora antes da listagem de usuarios acontecer tem um loading para verificar errors e busca de dados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,23 @@ import { ChakraProvider } from '@chakra-ui/react' //CRIANDO UM CONTEXTO DO CHAKR
 import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 import { makeServer } from '../services/mirage'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
 if(process.env.NODE_ENV === 'development'){// certificando que o servidor que esta rodando o app é de desenvolvimento
   makeServer();
 }
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,22 +1,25 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('http://localhost:3000/api/users')
+        const data = await response.json()
+
+        return data;
+    })// os dados serao salvos em cache local nessa chave
+    
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     })
-
-    useEffect(() => {
-        fetch('http://localhost:3000/api/users')
-        .then(response => response.json())
-        .then(data => console.log(data))
-    }, [])
 
     return (
         <Box>
@@ -41,6 +44,16 @@ export default function UserList() {
                         </Link>
                     </Flex>
 
+                    { isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários.</Text>
+                        </Flex>
+                    ) : (
+                    <>
                     <Table colorScheme="whiteAlpha">
                         <Thead>
                             <Tr>
@@ -127,6 +140,8 @@ export default function UserList() {
                     </Table>
 
                     <Pagination />
+                    </>
+                    )}
                 </Box>
             </Flex>
         </Box>
